0c5ed745483b86c717d60784c4f4ab1a4cdbef8d
Ivan Borisenko <mteru00@gmail.com>
2017-11-29 20:01:50 +0300

    + Добавлены тесты для страницы регистрации + Добавлено перенаправления для авторизованных пользователей + Обновлён прогресс требований

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class RegisterViewTest(TestCase):
+
+    def set_up(self):
+        """ Подготовка к тестам: создание веб-клиента """
+        self.client = Client()
+
+    def test_page_loads(self):
+        """ Проверяет загружается ли страница со статусом 200 (ОК) """
+        response = self.client.get('/register/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_page_template(self):
+        """ Проверяет используется ли шаблон register.html """
+        response = self.client.get('/register/')
+        self.assertTemplateUsed(response, 'accounts/register.html')
--- accounts/views.py
+++ accounts/views.py
@@ -8,7 +8,17 @@ from .models import User
 
 
 class RegistrationView(CreateView):
+    """ Страница содержащая форму регистрации """
+
     form_class = RegistrationForm
     success_url = reverse_lazy('home')
     model = User
     template_name = 'accounts/register.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        """ Перенаправить пользователя на главную страницу, если он уже вошел в систем """
+        
+        if request.user.is_authenticated:
+            return redirect('home')
+        else:
+            return super().dispatch(request, *args, **kwargs)
