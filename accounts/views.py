b566ed6e3a0a3dd7f0eb503156df0a784049e931
Ivan Borisenko <mteru00@gmail.com>
2017-11-29 19:42:56 +0300

    Исправление #2
    + Перенаправление после регистрации теперь работает исправно

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import PasswordResetForm
 from django.shortcuts import redirect
 from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
 from .forms import RegistrationForm
 from .models import User
@@ -8,11 +9,6 @@ from .models import User
 
 class RegistrationView(CreateView):
     form_class = RegistrationForm
-    success_url = '/login/'
+    success_url = reverse_lazy('home')
     model = User
     template_name = 'accounts/register.html'
-
-    def form_valid(self, form):
-        obj = form.save(commit=False)
-        obj.save()
-        return redirect(self.get_success_url())
