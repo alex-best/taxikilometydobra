8ed3d10dbfa7aaf1a4306ff2a21a272bc7bcb33f
Ivan Borisenko <mteru00@gmail.com>
2017-11-29 20:06:40 +0300

    Добавлены тесты для страницы входа
    + Добавлены стандартные тесты для страницы входа

--- accounts/tests.py
+++ accounts/tests.py
@@ -17,3 +17,20 @@ class RegisterViewTest(TestCase):
         """ Проверяет используется ли шаблон register.html """
         response = self.client.get('/register/')
         self.assertTemplateUsed(response, 'accounts/register.html')
+
+
+class LoginViewTest(TestCase):
+
+    def set_up(self):
+        """ Подготовка к тестам: создание веб-клиента """
+        self.client = Client()
+
+    def test_page_loads(self):
+        """ Проверяет загружается ли страница со статусом 200 (ОК) """
+        response = self.client.get('/login/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_page_template(self):
+        """ Проверяет используется ли шаблон login.html """
+        response = self.client.get('/login/')
+        self.assertTemplateUsed(response, 'accounts/login.html')
