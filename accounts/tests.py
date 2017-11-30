1e4f45ce2c3c6e257f2a8ae0a9bf60f025f7b8f8
Ivan Borisenko <mteru00@gmail.com>
2017-11-30 16:06:10 +0300

    Тесты для аутентификации добавлены

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,12 +1,9 @@
 from django.test import TestCase, Client
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 
 
 class RegisterViewTest(TestCase):
-
-    def set_up(self):
-        """ Подготовка к тестам: создание веб-клиента """
-        self.client = Client()
 
     def test_page_loads(self):
         """ Проверяет загружается ли страница со статусом 200 (ОК) """
@@ -21,10 +18,6 @@ class RegisterViewTest(TestCase):
 
 class LoginViewTest(TestCase):
 
-    def set_up(self):
-        """ Подготовка к тестам: создание веб-клиента """
-        self.client = Client()
-
     def test_page_loads(self):
         """ Проверяет загружается ли страница со статусом 200 (ОК) """
         response = self.client.get('/login/')
@@ -34,3 +27,27 @@ class LoginViewTest(TestCase):
         """ Проверяет используется ли шаблон login.html """
         response = self.client.get('/login/')
         self.assertTemplateUsed(response, 'accounts/login.html')
+
+
+class LoginTest(TestCase):
+    
+    def setUp(self):
+        """ Подготовка к тестам: создание пользователя и клиента """
+        self.credentials = {
+            'phone': '+1234567890',
+            'password': 'secret'
+        }
+        get_user_model().objects.create_user(**self.credentials)
+    
+    def test_user_exists(self):
+        """ Проверяет создан ли пользователь """
+        user = get_user_model().objects.get(phone = self.credentials['phone'])
+        self.assertEqual(user.phone, self.credentials['phone'])
+
+    def test_login(self):
+        """ Проверяет аутентификацию """
+        response = self.client.post('/login/', {
+            'username': '+1234567890',
+            'password': 'secret'
+        }, follow=True)
+        self.assertTrue(response.context['user'].is_authenticated)
